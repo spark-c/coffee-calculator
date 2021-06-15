1e0305fdf973d02514440464b3a66bc01565d800
Collin Sparks <cklsparks@gmail.com>
2021-06-15 16:39:24 -0500

    added basic calculation functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+// brewRatio = dry / water
+const calcBrewRatio = (dryCoffee, water) => {
+    return dryCoffee / water;
+};
+// water = dry / brewRatio
+const calcWater = (dryCoffee, brewRatio) => {
+    return dryCoffee / brewRatio;
+};
+// dry = brewRatio * water
+const calcDryCoffee = (brewRatio, water) => {
+    return brewRatio * water;
+};
+// brewedCoffee = water - 2(dry)
+const calcBrewedCoffee = (water, dryCoffee) => {
+    return (water - 2(dryCoffee));  
+};
+
 
 const Calculator = ({ params }) => {
     const values = params.map(param => {
